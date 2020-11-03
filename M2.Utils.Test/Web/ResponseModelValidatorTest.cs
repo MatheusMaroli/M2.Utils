33fe7e3d5bac866c7e4b162a17664fd198a00e8c
Matheus Maroli <matheus.maroli66@gmail.com>
2020-11-03 08:54:05 -0200

    ResponseModelValidator | ResponseModelValidatorTest

---
+++ M2.Utils.Test/Web/ResponseModelValidatorTest.cs
@@ -0,0 +1,34 @@
+﻿using M2.Utils.Web.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace M2.Utils.Test.Web
+{
+    public class ResponseModelValidatorTest
+    {
+        [Fact]
+        public void  Should_Be_Set_A_Error()
+        {
+            var response = new ResponseModelValidator();
+            response.SetError("Test", "Error Test");
+            Assert.True(response.Errors.Count == 1);
+        }
+
+        [Fact]
+        public void Should_Be_Valid_Response()
+        {
+            var response = new ResponseModelValidator();
+            Assert.True(response.IsValid());
+        }
+
+        [Fact]
+        public void Should_Be_Invalid_Response()
+        {
+            var response = new ResponseModelValidator();
+            response.SetError("Test", "Error Test");
+            Assert.True(! response.IsValid());
+        }
+    }
+}
--- M2.Utils.Web/Responses/ResponseModelValidator.cs
+++ M2.Utils.Web/Responses/ResponseModelValidator.cs
@@ -4,20 +4,18 @@ using M2.Utils.Web.Models;
 
 namespace M2.Utils.Web.Responses
 {
-    public class ResponseModelValidator : ResponseDefault
+    public class ResponseModelValidator 
     {
-        public IList<ErrorMessage> Errors {get;set;}
-        
-        public ResponseModelValidator() : base()
-        {
-            Errors = new List<ErrorMessage>();
-        }
+        public IList<ErrorMessage> Errors {get; private set;} = new List<ErrorMessage>();
 
         public void SetError(string propertyName, string message)
         {
-            Messages.Clear();
-            SetWarning("Invalid property formatter");
             Errors.Add(new ErrorMessage(){PropertyName = propertyName, Message = message});
+        }
+
+        public bool IsValid()
+        {
+            return Errors.Count <= 0;
         }
 
 
